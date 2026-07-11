9aca4a3bad0b05f56aabde7ae205b78e76351160
Deepika Barnikalaa <deepikabarnikala0410@gmail.com>
2026-07-11 12:16:11 +0530

    modules and it's types in python

---
+++ D17_firstModule.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def floor_(a,b):
+    return a//b
+def power_(a,b):
+    return a**b
+def modulo(a,b):
+    return a%b
+def cap(a,b):
+    return a^b
